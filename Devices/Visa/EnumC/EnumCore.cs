aed9af9acff382b864fd0ad904b6455c8a7568e2
Atelogoya <jesusmmoreno1@gmail.com>
2024-12-03 19:03:36 -0700

    Add files via upload

---
+++ Devices/Visa/EnumC/EnumCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devices.Visa.EnumC
+{
+    public static class EnumCore
+    {
+        public static string ToDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+
+            var result = attribute != null ? attribute.Description : string.Empty;
+
+            return result;
+        }
+    }
+}
