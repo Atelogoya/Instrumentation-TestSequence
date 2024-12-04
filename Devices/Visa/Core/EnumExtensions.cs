8143af0464e1491753c51bbfb6eb38e3b060191f
Atelogoya <jesusmmoreno1@gmail.com>
2024-12-03 18:59:38 -0700

    Add files via upload

---
+++ Devices/Visa/Core/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Devices.Visa.Core;
+
+public static class EnumExtensions
+{
+    public static string ToDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+
+        var result = attribute != null ? attribute.Description : string.Empty;
+
+        return result;
+    }
+}
